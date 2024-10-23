85cafa7b107a09015bae0f5722615c364680a941
Ahmed Moalla <ahmed.moalla@novencia.com>
2024-10-23 23:34:30 +0200

    Problem 15

--- src/main/java/com/amoalla/euler/utils/Algorithms.java
+++ src/main/java/com/amoalla/euler/utils/Algorithms.java
@@ -1,5 +1,6 @@
 package com.amoalla.euler.utils;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -66,6 +67,14 @@ public class Algorithms {
     }
 
     public static class Math {
+        public static BigInteger factorial(int n) {
+            BigInteger result = BigInteger.ONE;
+            for (int i = 2; i <= n; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+            return result;
+        }
+
         public static int gcd(int... numbers) {
             int result = numbers[0];
 
