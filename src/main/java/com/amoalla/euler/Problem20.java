363725e408f04f64f930ae95e63e3962dac52b41
Ahmed Moalla <ahmed.moalla@novencia.com>
2024-10-24 18:21:15 +0200

    Problem 20

---
+++ src/main/java/com/amoalla/euler/Problem20.java
@@ -0,0 +1,13 @@
+import static com.amoalla.euler.utils.Maths.factorial;
+
+/// # Factorial digit sum
+/// n! means n × (n − 1) × ... × 3 × 2 × 1.
+/// For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800, and the sum of the digits in the number 10! is
+/// 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+/// Find the sum of the digits in the number 100!
+void main() {
+    println(factorial(100).toString()
+            .chars()
+            .map(Character::getNumericValue)
+            .sum());
+}
